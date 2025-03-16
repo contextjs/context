7ef5ab50ac26a34c4d964add4ccdff63a6d19e0a
Ross Rotaru <rostislav@rotaru.me>
2025-03-15 19:47:42 -0700

    converted mts modules to js

--- scripts/build.js
+++ scripts/build.js
@@ -8,7 +8,7 @@
 
 import { execSync } from "child_process";
 import * as fs from 'fs';
-import { BUILD_FOLDER, PACKAGES_FOLDER, PROJECTS, VERSION } from "./config.mts";
+import { BUILD_FOLDER, PACKAGES_FOLDER, PROJECTS, VERSION } from "./config.js";
 
 try {
     npmInstall();
@@ -32,7 +32,7 @@ function npmInstall() {
     }
 }
 
-function buildProject(project: string) {
+function buildProject(project) {
     console.log(`Building "${project}"...`);
 
     removeDependency(project);
@@ -53,12 +53,12 @@ function createOutputDirectories() {
         fs.mkdirSync(PACKAGES_FOLDER);
 }
 
-function runLocalProjectBuild(project: string) {
-    if (fs.existsSync(`src/${project}/build.mts`))
-        execSync(`node src/${project}/build.mts`, { stdio: 'inherit' });
+function runLocalProjectBuild(project) {
+    if (fs.existsSync(`src/${project}/build.js`))
+        execSync(`node src/${project}/build.js`, { stdio: 'inherit' });
 }
 
-function setVersion(project: string) {
+function setVersion(project) {
     let packageContent = fs.readFileSync(`${BUILD_FOLDER}/${project}/package.json`)
         .toString()
         .replace(/__VERSION__/g, VERSION);
@@ -66,38 +66,38 @@ function setVersion(project: string) {
     fs.writeFileSync(`${BUILD_FOLDER}/${project}/package.json`, packageContent);
 }
 
-function copyPackageFile(project: string) {
+function copyPackageFile(project) {
     if (!fs.existsSync(`src/${project}/package.json`))
         throw new Error(`Missing package.json in "${project}" project.`);
     fs.copyFileSync(`src/${project}/package.json`, `${BUILD_FOLDER}/${project}/package.json`);
 }
 
-function createPackage(project: string) {
+function createPackage(project) {
     execSync(`cd src/${project} && tsc`, { stdio: 'inherit' });
     execSync(`cd ${BUILD_FOLDER}/${project} && npm pack --silent --pack-destination ../../${PACKAGES_FOLDER}`, { stdio: 'inherit' });
 }
 
-function installPackage(project: string) {
+function installPackage(project) {
     const packageJson = JSON.parse(fs.readFileSync(`${BUILD_FOLDER}/${project}/package.json`).toString());
     const name = packageJson.name.replace("@", "").replace("/", "-");
 
     execSync(`npm install ./${PACKAGES_FOLDER}/${name}-${VERSION}.tgz`, { stdio: 'inherit' });
 }
 
-function createDirectory(directory: string) {
+function createDirectory(directory) {
     if (fs.existsSync(directory))
         fs.rmSync(directory, { recursive: true });
     fs.mkdirSync(directory);
 }
 
-function getProjects(args: string[]) {
+function getProjects(args) {
     if (args.length > 0)
         return args;
 
     return PROJECTS;
 }
 
-function removeDependency(project: string) {
+function removeDependency(project) {
     console.log(`Removing dependency for "${project}"...`);
 
     const command = `npm pkg delete dependencies.@contextjs/${project}`;
