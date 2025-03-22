ed8545ce76036a0b84597ad4d6ebda1b26b45314
Ross Rotaru <rostislav@rotaru.me>
2025-03-21 19:58:08 -0700

    Added release script

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,4 +1,11 @@
 /**
+ * @license
+ * Copyright ContextJS All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found at https://github.com/context-js/context/blob/main/LICENSE
+ *
+ * 
  * This script builds ContextJS.
  * 
  * @example
