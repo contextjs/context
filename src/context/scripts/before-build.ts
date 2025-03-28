e5b3cd44cd9eb37847de6a1166a6f9127cf53c7f
Ross Rotaru <rostislav@rotaru.me>
2025-03-27 17:43:05 -0700

    Added and updated README files

---
+++ src/context/scripts/before-build.ts
@@ -0,0 +1,19 @@
+/**
+ * @license
+ * Copyright ContextJS All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found at https://github.com/contextjs/context/blob/main/LICENSE
+ */
+
+import Script from '../../../scripts/script.ts';
+
+export class BeforeBuild extends Script {
+    private readonly packageName: string = "context";
+
+    public override async runAsync(): Promise<void> {
+        await this.copyReadmeFileAsync(this.packageName);
+    }
+}
+
+await new BeforeBuild().runAsync();
