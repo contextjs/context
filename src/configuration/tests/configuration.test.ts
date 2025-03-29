48a6ba0ac059b292322e6e0bb1fd82a9e8452b19
Ross Rotaru <rostislav@rotaru.me>
2025-03-28 21:04:51 -0700

    Added license

--- src/configuration/tests/configuration.test.ts
+++ src/configuration/tests/configuration.test.ts
@@ -1,3 +1,11 @@
+/**
+ * @license
+ * Copyright ContextJS All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found at https://github.com/contextjs/context/blob/main/LICENSE
+ */
+
 import { Application } from "@contextjs/core";
 import { test, TestContext } from 'node:test';
 import { Configuration } from "../src/configuration.js";
--- src/configuration/tests/extensions/application-extensions.test.ts
+++ src/configuration/tests/extensions/application-extensions.test.ts
@@ -1,6 +1,14 @@
+/**
+ * @license
+ * Copyright ContextJS All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found at https://github.com/contextjs/context/blob/main/LICENSE
+ */
+
 import { Application } from "@contextjs/core";
 import { test, TestContext } from 'node:test';
-import '../../src/extensions/application-extensions.js';
+import '../../src/extensions/application-extensions.ts';
 
 test('Application: useConfiguration - success', (context: TestContext) => {
     const app = new Application();
