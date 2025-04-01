44938725f0cce30da37453926d176db4bbe8bf56
Ross Rotaru <rostislavrotaru@users.noreply.github.com>
2025-03-31 20:44:52 -0700

    Bump package versions to 0.4.0 in package.json and update version references in Config and VersionService (#62)

--- scripts/config.ts
+++ scripts/config.ts
@@ -7,7 +7,7 @@
  */
 
 export default class Config {
-    public static version: string = "0.3.0";
+    public static version: string = "0.4.0";
     public static buildFolder: string = "_build";
     public static packagesFolder: string = "_packages";
     public static packages: string[] = [
