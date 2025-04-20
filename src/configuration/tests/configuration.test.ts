3adad38b93b355d44560754ac0b73ce63ae5ab6a
Ross Rotaru <rostislav@rotaru.me>
2025-04-19 19:37:25 -0700

    feat(configuration): enhance README and tests, update package.json with keywords and exports
    Closes #127

--- src/configuration/tests/configuration.test.ts
+++ src/configuration/tests/configuration.test.ts
@@ -7,12 +7,12 @@
  */
 
 import { Application } from "@contextjs/system";
-import { test, TestContext } from 'node:test';
+import { test, TestContext } from "node:test";
 import { Configuration } from "../src/configuration.js";
-import '../src/extensions/application-extensions.js';
+import "../src/extensions/application-extensions.js";
 import { IConfigurationProvider } from "../src/interfaces/i-configuration.provider.js";
 
-test('Configuration - instance - success', (context: TestContext) => {
+test("Configuration: instance - success", (context: TestContext) => {
     const application = new Application();
     const configuration = new Configuration(application);
 
@@ -21,24 +21,23 @@ test('Configuration - instance - success', (context: TestContext) => {
     context.assert.strictEqual(configuration.useEnvironmentVariables, false);
 });
 
-test('Configuration - getValueAsync - success', async (context: TestContext) => {
+test("Configuration: getValueAsync - success", async (context: TestContext) => {
     class Provider implements IConfigurationProvider {
-        public load(source: any): IConfigurationProvider {
-            return this;
-        }
-
-        public async getValueAsync(key: string) {
+        async getValueAsync(key: string) {
             return key;
         }
     }
 
     const application = new Application();
     const provider = new Provider();
-    application.useConfiguration(options => { options.useProvider(provider); });
+
+    application.useConfiguration(options => {
+        options.useProvider(provider);
+    });
+
     const value = await application.configuration.getValueAsync("test");
 
     context.assert.strictEqual(value, "test");
-    context.assert.notStrictEqual(application.configuration, null);
     context.assert.strictEqual(application.configuration.providers.length, 1);
     context.assert.strictEqual(application.configuration.providers[0], provider);
 });
